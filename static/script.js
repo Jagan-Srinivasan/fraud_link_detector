f97778314f14faa7a94939be2b987df77c9ad520
JAGAN S <jaganrmkcet@gmail.com>
2025-07-07 14:23:37 +0530

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,1 +1,16 @@
+function checkURL() {
+  const url = document.getElementById("urlInput").value;
+  fetch('/check', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ url: url })
+  })
+  .then(res => res.json())
+  .then(data => {
+    document.getElementById("result").innerText = data.result;
+  })
+  .catch(() => {
+    document.getElementById("result").innerText = "Error checking the link.";
+  });
+}
 
